4b3ff1a943b1263bfaf7a203452911d86b601c58
Rahul Singh Dhek <rahulsinghdhek@gmail.com>
2019-02-02 11:58:49 +0530

    Update post_data.py

--- post_data.py
+++ post_data.py
@@ -9,5 +9,5 @@ start,end=2000,3000
 for i in range(start,end):
     data={}
     data['contact_name']=str(i)+"_"+chr(65+i%26)+chr(66+i%25)
-    data['email']=chr(122-i%26)+chr(121-i%25)+"."+email[i%len(email)]
+    data['email'] = chr(122 - i % 26) + chr(121 - i % 25) + "." + str(i)+ email[i % len(email)]
     requests.post(api_endpoint,data=data)
